fabca10d5c2bb0f912900a1fe8f2e45c92854765
sunsh1ny <grisha.sharov@gmail.com>
2025-11-11 15:26:02 +0300

    chore: add gh actions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './sass/style.scss';
 
-if (document.querySelector('.blog__list')) import('./js/posts.js');
-if (document.querySelector('.catalog__grid')) import('./js/products.js');
+import('./js/posts.js');
+import('./js/products.js');
 import './js/script.js';
