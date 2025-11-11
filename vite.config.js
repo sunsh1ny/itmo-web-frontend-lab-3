0298d845dbbf23fe87c352d0eae5544bd6900a73
sunsh1ny <grisha.sharov@gmail.com>
2025-11-11 13:37:09 +0300

    chore: add gh actions

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import {dirname, resolve} from "node:path";
+import {fileURLToPath} from "node:url";
+import {defineConfig} from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    base: "",
-    build: { rollupOptions: { input: {
+    base: "itmo-web-frontend-lab-3",
+    build: {
+        rollupOptions: {
+            input: {
                 main: resolve(__dirname, "index.html"),
                 catalog: resolve(__dirname, "catalog.scss.html"),
                 blog: resolve(__dirname, "blog.html"),
                 about: resolve(__dirname, "about.html"),
-            }}},
+            }
+        }
+    },
 });
